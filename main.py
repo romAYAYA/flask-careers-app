e30f18e2644ed86ff82ecc4d5894491d5cdf8629
Roman Khabayev <supernaturalmlady@gmail.com>
2023-10-23 17:58:30 +0600

    requirements were frozen. api jobs route added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -29,9 +29,15 @@ JOBS = [
     },
 ]
 
+
 @app.route('/')
 def hello_world():
     return render_template('home.html', jobs=JOBS, company_name='Jovian')
+
+
+@app.route('/api/jobs')
+def get_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
